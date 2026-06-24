2c0af2e556f9c827dbc5a69f5adc14b50afe7fbd
Aniruddha <aniruddha.santosh2001@gmail.com>
2026-06-24 15:26:05 +0530

    Updated

--- testautomation practice/DoubleClick.py
+++ testautomation practice/DoubleClick.py
@@ -1,6 +1,9 @@
 
-import timefrom selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
