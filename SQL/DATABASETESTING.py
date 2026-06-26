0cc204ae36ac7eaa4c4b4850e0df0f329f38ad18
Aniruddha <aniruddha.santosh2001@gmail.com>
2026-06-26 16:02:40 +0530

    Updated

--- SQL/DATABASETESTING.py
+++ SQL/DATABASETESTING.py
@@ -1,31 +1,38 @@
-
-import re #Regular Expressions
-
+# To Open Chrome and launch the website
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+# To Use WebdriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# To Connect To MySQL Server
+import mysql.connector
+
+# To Use By. and select class
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-import mysql.connector
-
-import time
+# For Regular Expressions
+import re
 
 
 serv_obj=Service(ChromeDriverManager().install())
+
 driver=webdriver.Chrome(service=serv_obj)
-driver.implicitly_wait(10)
 driver.maximize_window()
 
 driver.get(r"https://www.sbisecurities.in/calculators/fd-calculator")
 
+wait=WebDriverWait(driver,10)
 
-connection=mysql.connector.connect(host="localhost",user="root",passwd="admin",database="cal")
-cursor=connection.cursor() #create cursor
-cursor.execute("select * from caldata") #execute query through cursor
+connection=mysql.connector.connect(host="localhost",user="root",password="admin",database="cal")
+cursor=connection.cursor() # Create cursor
+cursor.execute("select * from caldata") # Execute Query through cursor
 
+# Reading data from excel
 for row in cursor:
-        #reading data from excel
     price=row[0]
     roi=row[1]
     per1=row[2]
@@ -33,24 +40,31 @@ for row in cursor:
     freq=row[4]
     exp_mvalue=row[5]
 
-    #passing data to application
-    driver.find_element(By.XPATH, "//div[@class='input-group']//input[@id='input_fd_investment']").send_keys(price)
-    driver.find_element(By.XPATH, "//input[@id='input_interest']").send_keys(roi)
-    driver.find_element(By.XPATH, "//body/app-root/app-fd-calculator[@class='ng-star-inserted']/div[@class='container']/div[@class='col-md-12 pt-5 mt-5']/div[@class='row pdt-30']/div[@class='col-xl-9 col-lg-7 col-md-7']/div[@class='border']/form[@id='validForm']/div[@class='row mt-2 d-flex']/div[@class='form-group col-md-6 mb-3']/input[1]").send_keys(per1)
-    comp_freq=Select(driver.find_element(By.XPATH, "//select[@id='frequency']"))
+    # Passing data to Application
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='fd_investment']"))).send_keys(price)
+
+    wait.until(EC.visibility_of_element_located((By.ID, "input_interest"))).send_keys(roi)
+
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Enter Number of Years']"))).send_keys(per1)
+
+    freq_elem=wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "select[id='frequency']")))
+    comp_freq=Select(freq_elem)
     comp_freq.select_by_visible_text(per2)
-    driver.find_element(By.XPATH, "//div[@class='form-group col-md-6 mb-3 align']").click()
-    act_mvalue=driver.find_element(By.XPATH, "//div[@class='border']//p[1]").text
-    act_clean=re.sub(r"[^\d]", "", act_mvalue)    #keeps only digits
 
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group col-md-6 mb-3 align']"))).click()
 
-    #validation
-    if int(act_clean)==int(exp_mvalue):
-       print("Test Passed")
+    act_mvalue=wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='border']//p[1]"))).text
+    act_clean = re.sub(r"[^\d]", "", act_mvalue)  # keeps only digits
+
+    # validation
+    if int(act_clean) == int(exp_mvalue):
+        print("test Passed")
     else:
         print("Test Failed")
 
-    driver.find_element(By.XPATH, "//a[@class='btn sidebar-btn w-100 current-page']").click()
-    time.sleep(2)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='btn sidebar-btn w-100 current-page']"))).click()
 
 connection.close()
+
+
+
